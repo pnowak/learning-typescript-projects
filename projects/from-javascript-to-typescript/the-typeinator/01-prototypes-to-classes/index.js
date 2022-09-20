7d1697110b987814c82e204418d57999168d180e
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-09-20 13:44:25 +0200

    Add solution for typeinator -> 01

--- projects/from-javascript-to-typescript/the-typeinator/01-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-prototypes-to-classes/index.js
@@ -1,5 +1,59 @@
 // Put your Robot and Humanoid classes here! ✨
 // See ./original.js for their older JavaScript code.
 
+class Robot {
+	name;
+	abilities;
+	power = 100;
+
+	constructor(name, abilities) {
+		this.name = name;
+		this.abilities = abilities;
+	}
+
+	announce() {
+		console.log("Greetings. I am " + this.name + ".");
+
+		for (var i = 0; i < this.abilities.length; i += 1) {
+			console.log("I am able to " + this.abilities[i] + ".");
+		}
+	}
+
+	charge(amount) {
+		if (this.power < 100) {
+			this.power = Math.min(this.power + amount, 100);
+			console.log("Recharged power supplies to " + this.power + ".");
+		}
+
+		if (this.power === 100) {
+			console.log("I am at optimal operational capacity.");
+		}
+	}
+
+	move(distance) {
+		if (this.power < distance) {
+			console.log("I do not have enough power to move " + distance + " units.");
+		} else {
+			console.log("Moving " + distance + " units.");
+			this.power -= distance;
+		}
+	}
+}
+
+class Humanoid extends Robot {
+	name;
+	abilities;
+	catchphrase;
+
+	constructor(name, abilities, catchphrase) {
+		super(name, abilities);
+		this.catchphrase = catchphrase;
+	}
+
+	announce() {
+		console.log(" > " + this.catchphrase + " <");
+	}
+}
+
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
