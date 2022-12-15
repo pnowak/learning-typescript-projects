3fc669c54f7b3fc3e4d74a2fe581694af4cc65f6
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-12-15 18:15:19 +0100

    Solutions for 1. Incoming Cipher from Secret Secrets/Functions

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,19 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+type Cipher = (text: string) => string;
+
+type CreateCipher = (cipher: Cipher) => Cipher;
+
+export const createCipher: CreateCipher = (cipher) => {
+	const cipherCurrying = (text: string) => {
+		const arrayOfString: string[] = [];
+
+		for (const t of text) {
+			arrayOfString.push(cipher(t));
+		}
+
+		return arrayOfString.join("");
+	};
+
+	return cipherCurrying;
+};
