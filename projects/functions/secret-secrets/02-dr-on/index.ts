f5830bff38d6d4a7fd53fb313ef64f21a4c7c46f
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-12-16 12:03:30 +0100

    Solutions for 2. Dr. On  from Secret Secrets/Functions

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,30 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type Cipher = (character: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: Cipher,
+	onConsonant: Cipher,
+	onPunctuation: Cipher
+) {
+	return (text: string) => {
+		let result = "";
+
+		for (const char of text) {
+			switch (true) {
+				case /[aeiou]/.test(char):
+					result += onVowel(char);
+					break;
+				case /[bcdfghjklmnpqrstvwxyz]/.test(char):
+					result += onConsonant(char);
+					break;
+				default:
+					result += onPunctuation(char);
+					break;
+			}
+		}
+
+		return result;
+	};
+}
