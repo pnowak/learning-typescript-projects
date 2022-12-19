704de88435679e9256d5ef874960d8d5d851a279
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-12-19 15:20:09 +0100

    Solutions for 3. The Golden Code from Secret Secrets/Functions

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,31 @@
 // Write your createCodeCracker function here! ✨
 // You'll need to export it so the tests can run it.
+export type MakeGuess = (text: string, attempt: number) => string;
+export type ValidateGuess = (guess: string) => boolean;
+
+export type Options = {
+	attempts: number;
+	makeGuess: MakeGuess;
+	validateGuess: ValidateGuess;
+};
+
+export function createCodeCracker(options: Options) {
+	const { attempts, makeGuess, validateGuess } = options;
+
+	return function createCodeCrackerRecursive(
+		text: string,
+		attempt = 0
+	): string | undefined {
+		if (attempts > attempt) {
+			const makeGuestResult = makeGuess(text, attempt);
+
+			const result = validateGuess(makeGuestResult)
+				? makeGuestResult
+				: undefined;
+
+			return result !== undefined
+				? result
+				: createCodeCrackerRecursive(text, (attempt += 1));
+		}
+	};
+}
