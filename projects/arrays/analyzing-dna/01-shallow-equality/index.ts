cd6cb9b42cb6053848a1065a89fecc648f91d1b0
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-12-20 17:18:22 +0100

    Solutions for 1. Shallow Equality from Analyzing DNA/Arrays

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,17 @@
 // Write your shallowEquality function here! ✨
 // You'll need to export it so the tests can run it.
+type ShallowEquality = (a: string[], b: string[]) => boolean;
+
+export const shallowEquality: ShallowEquality = (a, b) => {
+	if (a.length !== b.length) {
+		return false;
+	}
+
+	for (let indexA = 0; indexA < a.length; indexA += 1) {
+		if (a[indexA] !== b[indexA]) {
+			return false;
+		}
+	}
+
+	return true;
+};
