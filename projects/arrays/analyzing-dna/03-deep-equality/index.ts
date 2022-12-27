2ed0ae4b5d8363d9f4f906953dfde0ce0127d8b8
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2022-12-27 13:41:08 +0100

    Solutions for 3. Deep Equality from Analyzing DNA/Arrays

--- projects/arrays/analyzing-dna/03-deep-equality/index.ts
+++ projects/arrays/analyzing-dna/03-deep-equality/index.ts
@@ -1,2 +1,23 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+type DeepEquality = (a: string[][], b: string[][]) => boolean;
+
+export const deepEquality: DeepEquality = (a, b) => {
+	if (a.length !== b.length) {
+		return false;
+	}
+
+	for (let indexA = 0; indexA < a.length; indexA += 1) {
+		if (a[indexA].length !== b[indexA].length) {
+			return false;
+		}
+
+		for (let indexAA = 0; indexAA < a[indexA].length; indexAA += 1) {
+			if (a[indexA][indexAA] !== b[indexA][indexAA]) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+};
