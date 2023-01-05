be6381d5c1f258483079a0572e9c54736107894b
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-05 09:56:51 +0100

    Solutions for 1. Cities from Vacation Planning/Interfaces

--- projects/interfaces/vacation-planning/01-cities/index.ts
+++ projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,39 @@
 // Write your describeCity function here! ✨
 // You'll need to export it so the tests can run it.
+type Coordinate = [number, number, number];
+
+export interface City {
+	coordinates: {
+		north: Coordinate;
+		west: Coordinate;
+	};
+	name: string;
+	catchphrase?: string;
+}
+
+interface CoordinateMap {
+	[key: number]: string;
+}
+
+const coordinateMap: CoordinateMap = {
+	0: "°",
+	1: "'",
+	2: '"',
+} as const;
+
+const reduceCoordinate = (coordinate: Coordinate, direction: "N" | "W") =>
+	coordinate.reduce<Record<number, string>>(
+		(accumulator, currentValue, currentIndex) =>
+			`${accumulator}` +
+			`${currentValue}` +
+			`${coordinateMap[currentIndex]}${direction}`,
+		``
+	);
+
+export const describeCity = (city: City) =>
+	`${city.name}, New York
+    ${city.catchphrase ? `* "${city.catchphrase}"` : ""}
+    * Located at ${reduceCoordinate(
+			city.coordinates.north,
+			"N"
+		)} ${reduceCoordinate(city.coordinates.west, "W")}`;
