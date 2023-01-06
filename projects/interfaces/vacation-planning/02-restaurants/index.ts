c4e705ea309dd97589ad6876ca729375b502d4f8
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-06 18:20:11 +0100

    Solutions for 2. Restaurants from Vacation Planning/Interfaces

--- projects/interfaces/vacation-planning/02-restaurants/index.ts
+++ projects/interfaces/vacation-planning/02-restaurants/index.ts
@@ -1,2 +1,19 @@
 // Write your groupLandmarks function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface CityRestaurants {
+	city: string;
+	name: string;
+}
+
+export const groupRestaurants = (cityRestaurants: CityRestaurants[]) => {
+	return cityRestaurants.reduce<Record<string, string[]>>(
+		(accumulator, cityObject) => {
+			const key = cityObject.city;
+			const currentGroup = accumulator[key] ?? [];
+
+			return { ...accumulator, [key]: [...currentGroup, cityObject.name] };
+		},
+		{}
+	);
+};
