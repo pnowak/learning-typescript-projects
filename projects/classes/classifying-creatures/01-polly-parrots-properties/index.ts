d0dffb73ca7deaf91e069a88cc8cb29a49c77892
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-10 09:03:37 +0100

    Solutions for 1. Polly Parrots Properties from Classifying Creatures/Classes

--- projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
+++ projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
@@ -1,2 +1,23 @@
 // Write your Parrot class here! ✨
 // You'll need to export it so the tests can run it.
+export class Parrot {
+	#name: string;
+	#phrases: string[];
+
+	constructor(name?: string) {
+		this.#name = name ?? "Polly";
+		this.#phrases = [`${this.#name} wants a cracker!`];
+	}
+
+	announce() {
+		return `Squawk! I'm ${this.#name}!`;
+	}
+
+	learn(additionalPhrase: string) {
+		this.#phrases.push(additionalPhrase);
+	}
+
+	speak() {
+		return this.#phrases[Math.floor(Math.random() * this.#phrases.length)];
+	}
+}
