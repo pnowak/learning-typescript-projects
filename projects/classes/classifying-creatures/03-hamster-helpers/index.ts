8ebc8eccc850ba16c5ac0371ff3b90b7bc20fc77
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-19 09:50:58 +0100

    Solutions for 3. Hamster Helpers from Classifying Creatures/Classes

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,52 @@
 // Write your type and classes here! ✨
 // You'll need to export the classes so the tests can run them.
+export type SmallPetFoot =
+	| "bugs"
+	| "fruits"
+	| "insects"
+	| "plants"
+	| "seeds"
+	| "vegetables";
+
+abstract class SmallFurryPet {
+	readonly species: string;
+	happiness = 0;
+
+	constructor(species: string) {
+		this.species = species;
+	}
+
+	abstract eats(food: SmallPetFoot): boolean;
+
+	isHappy() {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	eats(food: SmallPetFoot): boolean {
+		return ["insects", "plants", "seeds", "vegetables"].includes(food);
+	}
+
+	dig() {
+		this.happiness += 1;
+	}
+}
+
+export class Hamster extends SmallFurryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	eats() {
+		return true;
+	}
+
+	run() {
+		this.happiness += 1;
+	}
+}
