b4fa1e9cbb6ce2aee17f92fc7e5f9f22b27d4069
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-25 08:23:01 +0100

    Solutions for 2. Zip from Hidash/Generics

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,22 @@
 // Write your zip function here! ✨
 // You'll need to export it so the tests can run it.
+export const zip = <TItem1, TItem2>(array1: TItem1[], array2: TItem2[]) => {
+	const result: (TItem1 | TItem2)[] = [];
+
+	const len = array1.length > array2.length ? array1.length : array2.length;
+	let i = 0;
+
+	while (i < len) {
+		if (array1[i]) {
+			result.push(array1[i]);
+		}
+
+		if (array2[i]) {
+			result.push(array2[i]);
+		}
+
+		i += 1;
+	}
+
+	return result;
+};
