d4146e6a691bd22cce00a6507cdcc4cdb2a36ad1
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-30 09:32:51 +0100

    Solutions for 1. Unique from Hidash/Generics

--- projects/generics/hidash/01-unique/index.ts
+++ projects/generics/hidash/01-unique/index.ts
@@ -1,2 +1,15 @@
 // Write your unique function here! ✨
 // You'll need to export it so the tests can run it.
+export const unique = <TItem>(...items: TItem[][]) => {
+	const result: TItem[] = [];
+
+	for (let i = 0; i < items.length; i += 1) {
+		for (let j = 0; j < items[i].length; j += 1) {
+			if (!result.includes(items[i][j])) {
+				result.push(items[i][j]);
+			}
+		}
+	}
+
+	return result;
+};
