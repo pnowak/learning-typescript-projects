133801206975f371f2e4c6aa30b08c2c59e6427c
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-01-31 10:44:51 +0100

    Solutions for 3. Pick from Hidash/Generics

--- projects/generics/hidash/03-pick/index.ts
+++ projects/generics/hidash/03-pick/index.ts
@@ -1,2 +1,10 @@
 // Write your pick function here! ✨
 // You'll need to export it so the tests can run it.
+type Primitive = string | number | bigint | boolean | symbol | undefined | null;
+
+export function pick<
+	TContainer extends { [key: string]: Primitive },
+	TKey extends keyof TContainer
+>(container: TContainer, key: TKey) {
+	return container[key];
+}
