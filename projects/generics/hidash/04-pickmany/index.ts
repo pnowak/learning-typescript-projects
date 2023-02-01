20aae41411505f5a43bb0201724f131168804da7
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-02-01 09:59:38 +0100

    Solutions for 4. Pick Many from Hidash/Generics

--- projects/generics/hidash/04-pickmany/index.ts
+++ projects/generics/hidash/04-pickmany/index.ts
@@ -1,2 +1,14 @@
 // Write your pickMany function here! ✨
 // You'll need to export it so the tests can run it.
+export function pickMany<
+	TContainer extends object,
+	TKeys extends keyof TContainer
+>(container: TContainer, keys: TKeys[]) {
+	const result: TContainer[TKeys][] = [];
+
+	for (const key of keys) {
+		result.push(container[key]);
+	}
+
+	return result;
+}
