cef5226b55fe06ee4077fbab898fbf4e4d83ae0f
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-02-10 13:57:34 +0100

    Solutions for 1. Passport Declarations from Global Travels/Declaration Files

--- projects/declaration-files/global-travels/01-passport-declarations/index.ts
+++ projects/declaration-files/global-travels/01-passport-declarations/index.ts
@@ -1,4 +1,18 @@
 // Write your types here! ✨
+declare global {
+	function checkPassport(id: string): CheckPassportReturnType;
+}
+
+interface CheckPassportDontAllowed {
+	allowed: false;
+	reason: "No passport found." | "No known name." | "Passport has expired.";
+}
+
+interface CheckPassportAllowed {
+	allowed: true;
+}
+
+type CheckPassportReturnType = CheckPassportDontAllowed | CheckPassportAllowed;
 
 export function checkPassport(id: string) {
 	const passport = window.passports[id];
