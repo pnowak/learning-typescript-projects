7418df0e9df9cb134668da605d0e634970b3abf9
“pnowak” <piotr.nowak.25.02.1978@gmail.com>
2023-03-10 08:35:55 +0100

    Solutions for 1. SpOnGeCaSe from Template Literal Type Shenanigans/Type Operations

--- projects/type-operations/template-literal-type-shenanigans/01-spongecase/index.ts
+++ projects/type-operations/template-literal-type-shenanigans/01-spongecase/index.ts
@@ -1,2 +1,12 @@
 // Write your SpOnGeCaSe type here! ✨
 // You'll need to export it so the tests can run it.
+export type SpOnGeCaSe<
+	Text,
+	FirstTransform extends "upper" | "lower" = "upper"
+> = Text extends `${infer Prefix}${infer Rest}`
+	? FirstTransform extends "upper"
+		? `${Capitalize<Prefix>}${SpOnGeCaSe<Rest, "lower">}`
+		: FirstTransform extends "lower"
+		? `${Lowercase<Prefix>}${SpOnGeCaSe<Rest>}`
+		: never
+	: Text;
